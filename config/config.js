156b2b1ac5e0244a6b33a8fa2b484dfe8f661690
ChurchillG <churchillg95@gmail.com>
2026-04-28 18:01:10 +0200

    config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'https://www.ndosiautomation.co.za/APIDEV';
+
+export const USERS = {
+    validUser: {
+        email: 'spare@admin.com',
+        password: '@12345678'
+    }
+};
