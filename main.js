579157d903b0740de0c1fc13712eb833cca92b25
ChurchillG <churchillg95@gmail.com>
2026-04-28 18:29:51 +0200

    modified main.js to add a report

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 import { userFlow } from './scenarios/userFlow.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options = {
     vus: 5,
     duration: '20s',
+    thresholds: {
+        http_req_duration: ['p(95)<500'],
+        http_req_failed: ['rate<0.01']
+    }
 };
 
 export default function () {
     userFlow();
+}
+
+// Generate HTML report
+export function handleSummary(data) {
+    return {
+        "report.html": htmlReport(data),
+    };
 }
