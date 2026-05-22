3f18bea1d52a5f840e4def68a7b54b85f45bad1b
ChurchillG <churchillg95@gmail.com>
2026-05-22 16:13:41 +0200

    updated checks

--- utils/checks.js
+++ utils/checks.js
@@ -1,7 +1,7 @@
 import { check } from 'k6';
 
 export function checkResponse(res, name) {
-    // Accept both 200 and 201 as successful
+   
     const isSuccessful = check(res, {
         [`${name} - status is 200 or 201`]: (r) => r.status === 200 || r.status === 201
     });
